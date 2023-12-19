f53f1505589af32c9452058f4b539c107850ffb0
devangardesana <ardesanadevang@gmail.com>
2023-12-19 17:54:10 -0500

    optimise import

--- src/component/Screen1.tsx
+++ src/component/Screen1.tsx
@@ -1,4 +1,4 @@
-import {Button, Text, View} from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {TAB1, TAB2} from '../Util/Constant.ts';
--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,10 +1,6 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Screen1 from '../component/Screen1.tsx';
-import {SCREEN1, SCREEN2, TAB1, TAB2} from '../Util/Constant.ts';
 import Screen2 from '../component/Screen2.tsx';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Tab1 from '../component/Tab1.tsx';
-import Tab2 from '../component/Tab2.tsx';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 
